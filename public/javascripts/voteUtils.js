a22ea244d1f1c2b1972a96593552a964e10a2f11
Chung <63246320+Chung1045@users.noreply.github.com>
2024-10-14 21:22:52 +0800

    New styling for disabled options

--- public/javascripts/voteUtils.js
+++ public/javascripts/voteUtils.js
@@ -1,18 +1,5 @@
-function initializeDynamicButtons() {
-    const buttons = document.querySelectorAll('.text_voteOptions_p');
+const fs = require('fs');
 
-    buttons.forEach(button => {
-        function clickHandler() {
-            const buttonId = this.getAttribute('data-option-id');
-            const parentId = this.closest('.card').getAttribute('data-vote-id');
-            alert(`You are now on VoteID ${parentId}\nYou clicked option ${buttonId}`);
-            this.classList.add('clicked_selected');
-            button.removeEventListener('click', clickHandler);
-        }
+function voteForOptions(voteID, voteOptions){
 
-        button.addEventListener('click', clickHandler);
-    });
 }
-
-
-document.addEventListener('DOMContentLoaded', initializeDynamicButtons);
