f9e35766da281691c7d1ce9546be112fa0780b71
Chung <63246320+Chung1045@users.noreply.github.com>
2024-11-07 20:48:01 +0800

    Refined codebase to make it more modular and easier to maintain, added the ability to remove option when the number of options is 3 or above, other minor changes

--- public/javascripts/dashboardUtils.js
+++ public/javascripts/dashboardUtils.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // Initialize Masonry
     var $grid = $('#container').masonry({
         itemSelector: '.card',
@@ -7,39 +7,92 @@ $(document).ready(function(){
         fitWidth: true
     });
 
-    $(document).on('click', '#btn-delete', function(){
+    // Function to check the number of options and toggle delete buttons
+    function checkOptionsCount(optionsList) {
+        const totalOptions = optionsList.children('.option-wrapper').length;
+        if (totalOptions < 3) {
+            optionsList.find('.delete-option-icon').hide();
+        } else {
+            optionsList.find('.delete-option-icon').show();
+        }
+    }
+
+    // Handle delete button click
+    $(document).on('click', '#btn-delete', function() {
         let voteEntry = $(this).closest('.card');
         let voteID = voteEntry.attr('data-vote-id');
+        let parentElement = $(this).closest('.card');
+        const optionsList = parentElement.find(`#vote_option_list[data-vote-id="${voteID}"]`);
         $('#ModalDialogue').modal('show');
         $('#btn-modal-confirm').data('vote-id', voteID).data('action', 'delete');
+        checkOptionsCount(optionsList);
     });
 
-    $(document).on('click', '.editable_addOptions', function(){
+    // Handle "Add new options" button click
+    $(document).on('click', '.editable_addOptions', function() {
         let parentElement = $(this).closest('.card');
         let voteID = parentElement.attr('data-vote-id');
-
-        // Find the options list within the card
         const optionsList = parentElement.find(`#vote_option_list[data-vote-id="${voteID}"]`);
-
-        // Create the new editable option
-        let newOption = $('<div contenteditable />').addClass('editable_options').text('New option');
-
-        // Append the new option to the options list
-        optionsList.append(newOption);
-
-        // Trigger Masonry layout to adjust the new card size
+        addNewOption(optionsList);
         $grid.masonry('layout');
+        checkOptionsCount(optionsList);
+    });
+
+    // Handle edit button click
+    $(document).on('click', '#btn-edit', function() {
+        let parentElement = $(this).closest('.card');
+        let voteID = parentElement.attr('data-vote-id');
+        makeVoteEditable(parentElement, voteID);
+        $grid.masonry('layout');
+        const optionsList = parentElement.find(`#vote_option_list[data-vote-id="${voteID}"]`);
+        checkOptionsCount(optionsList);
+    });
+
+    // Handle confirmation of delete action
+    $(document).on('click', '#btn-modal-confirm', function() {
+        let voteID = $(this).data('vote-id');
+        if (voteID !== undefined) {
+            if ($(this).data('action') === 'delete') {
+                let voteEntry = document.querySelector(`.card[data-vote-id="${voteID}"]`);
+                $(voteEntry).remove();
+                $grid.masonry('layout');
+            }
+        }
+    });
+
+    // Function to add a new editable option
+    function addNewOption(optionsList) {
+        // Create the new editable option
+        let newOptionWrapper = $('<div/>').addClass('option-wrapper');
+        let newOption = $('<div contenteditable />').addClass('editable_options').text('New option');
+        let deleteIcon = $('<img/>').attr('src', '/images/icns/trash-fill.svg').addClass('delete-option-icon');
+
+        // Append the option and the delete icon to the wrapper
+        newOptionWrapper.append(newOption, deleteIcon);
+        optionsList.append(newOptionWrapper);
 
         // Handle input for the new option
         newOption.on('input', function() {
             $grid.masonry('layout'); // Recalculate layout when input changes
         });
-    });
 
-    $(document).on('click', '#btn-edit', function(){
-        let parentElement = $(this).closest('.card');
-        let voteID = parentElement.attr('data-vote-id');
+        deleteIcon.on('click', function() {
+            newOptionWrapper.remove();
+            $grid.masonry('layout'); // Update Masonry layout
+            checkOptionsCount(optionsList);
+        });
 
+        newOption.on('focus', function() {
+            newOptionWrapper.addClass('focused');
+        });
+
+        newOption.on('blur', function() {
+            newOptionWrapper.removeClass('focused');
+        });
+    }
+
+    // Function to make a vote editable
+    function makeVoteEditable(parentElement, voteID) {
         const editButton = parentElement.find('#btn-edit');
         const title = parentElement.find(`#text_voteTitle_h2[data-vote-id="${voteID}"]`);
         const optionsList = parentElement.find(`#vote_option_list[data-vote-id="${voteID}"]`);
@@ -55,36 +108,13 @@ $(document).ready(function(){
         let horizontalRule = $('<hr/>').addClass("editable_hr");
 
         // Make title editable
-        let titleText = title.text();
-        let originalClasses = title.attr('class');
-        let originalStyles = title.attr('style');
-        let originalMargin = title.css('margin');
-
-        let textareaElement = $('<div contenteditable />').text(titleText);
-        title.replaceWith(textareaElement);
-        textareaElement.attr('class', originalClasses)
-            .attr('style', originalStyles)
-            .css('margin', originalMargin)
-            .addClass('editable_title')
-            .css({'display': 'block', 'width': '100%'});
+        makeElementEditable(title, parentElement);
 
         // Make options editable
         options.each(function() {
-            let currentText = $(this).text();
-            let originalStyles = $(this).attr('style');
-            let originalMargin = $(this).css('margin');
-            let optionId = $(this).attr('data-option-id');
-
-            let inputElement = $('<div contenteditable />').text(currentText);
-            inputElement.attr('class', 'editable_options')
-                .attr('style', originalStyles)
-                .css('margin', originalMargin)
-                .attr('data-option-id', optionId)
-                .attr('data-vote-id', voteID)
-                .css({'display': 'block'});
-
-            $(this).replaceWith(inputElement);
+            makeOptionEditable($(this), voteID);
         });
+
 
         // Append the "Add new options" button after the options list
         divEditOptions.append(horizontalRule, newOptionsBt);
@@ -93,28 +123,67 @@ $(document).ready(function(){
         // Change edit button icon
         editButton.attr('src', '/images/icns/check2.svg');
 
-        // Update Masonry layout
-        $grid.masonry('layout');
+        // Check and update delete button visibility
+        checkOptionsCount(optionsList);
+    }
 
-        // Handle input events for dynamic layout
+    // Function to make an element editable
+    function makeElementEditable(element, parentElement) {
+        let originalText = element.text();
+        let originalClasses = element.attr('class');
+        let originalStyles = element.attr('style');
+        let originalMargin = element.css('margin');
+
+        let textareaElement = $('<div contenteditable />').text(originalText);
+        element.replaceWith(textareaElement);
+        textareaElement.attr('class', originalClasses)
+            .attr('style', originalStyles)
+            .css('margin', originalMargin)
+            .addClass('editable_title')
+            .css({ 'display': 'block', 'width': '100%' });
+
+        // Restore the original title styles
+        textareaElement.css({
+            'font-size': parentElement.find('#text_voteTitle_h2').css('font-size'),
+            'font-weight': parentElement.find('#text_voteTitle_h2').css('font-weight'),
+            'color': parentElement.find('#text_voteTitle_h2').css('color')
+        });
+
         textareaElement.on('input', function() {
             $grid.masonry('layout');
         });
+    }
 
-        parentElement.find('.editable_options').on('input', function() {
+    // Function to make an option editable
+    function makeOptionEditable(option, voteID) {
+        let currentText = option.text();
+        let originalStyles = option.attr('style');
+        let optionId = option.attr('data-option-id');
+
+        let optionWrapper = $('<div/>').addClass('option-wrapper');
+        let inputElement = $('<div contenteditable />').text(currentText);
+        let deleteIcon = $('<img/>').attr('src', '/images/icns/trash-fill.svg').addClass('delete-option-icon');
+
+        inputElement.attr('class', 'editable_options')
+            .attr('style', originalStyles)
+            .attr('data-option-id', optionId)
+            .attr('data-vote-id', voteID)
+            .css({ 'display': 'block' });
+
+        optionWrapper.append(inputElement, deleteIcon);
+        option.replaceWith(optionWrapper);
+
+        // Handle delete option click
+        deleteIcon.on('click', function() {
+            const parentElement = $(this).closest('.card');
+            const optionsList = parentElement.find(`#vote_option_list[data-vote-id="${voteID}"]`);
+            optionWrapper.remove();
+            checkOptionsCount(optionsList);
+            $grid.masonry('layout'); // Update Masonry layout
+        });
+
+        inputElement.on('input', function() {
             $grid.masonry('layout');
         });
-    });
-
-    $(document).on('click', '#btn-modal-confirm', function(){
-        let voteID = $(this).data('vote-id'); // Retrieve voteID from data attribute
-        if (voteID !== undefined) {
-            if ($(this).data('action') === 'delete') {
-                let voteEntry = document.querySelector(`.card[data-vote-id="${voteID}"]`);
-                $(voteEntry).remove();
-                $grid.masonry('layout');
-            }
-        }
-    });
-
+    }
 });
